7a74999837d750b2be0bf0bc808085d53dfdc099
autumn <1764608405@qq.com>
2023-06-02 21:32:59 +0800

    文章浏览量redis更新至数据库、文章详情浏览量读取redis实时更新

---
+++ autumn-blog/src/main/java/com/autumn/job/UpdateViewCountJob.java
@@ -0,0 +1,41 @@
+package com.autumn.job;
+
+import com.autumn.domain.entity.Article;
+import com.autumn.service.ArticleService;
+import com.autumn.utils.RedisCache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @Author: qiuqiuya
+ * @Description:
+ * @Date: 2023/6/2 21:16
+ */
+@Component
+public class UpdateViewCountJob {
+
+    @Autowired
+    private RedisCache redisCache;
+
+    @Autowired
+    private ArticleService articleService;
+
+    @Scheduled(cron = "0 0 3,15 * * ? ")
+    public void updateViewCount(){
+        //获取redis中的浏览量
+        Map<String, Integer> viewCountMap = redisCache.getCacheMap("article:viewCount");
+
+        List<Article> articles = viewCountMap.entrySet()
+                .stream()
+                .map(entry -> new Article(Long.valueOf(entry.getKey()), entry.getValue().longValue()))
+                .collect(Collectors.toList());
+        //更新到数据库中
+        articleService.updateBatchById(articles);
+
+    }
+}
