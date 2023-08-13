0097b3275b65f251c5d9e5d8cb21963a56a3ab72
秋秋鸭 <1764608405@qq.com>
2023-08-13 15:42:39 +0800

    admin Excel导出

--- autumn-admin/src/main/java/com/autumn/controllter/CategoryController.java
+++ autumn-admin/src/main/java/com/autumn/controllter/CategoryController.java
@@ -1,13 +1,21 @@
 package com.autumn.controllter;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson.JSON;
 import com.autumn.domain.ResponseResult;
+import com.autumn.domain.entity.Category;
 import com.autumn.domain.vo.CategoryVo;
+import com.autumn.domain.vo.ExcelCategoryVo;
+import com.autumn.enums.AppHttpCodeEnum;
 import com.autumn.service.CategoryService;
+import com.autumn.utils.BeanCopyUtils;
+import com.autumn.utils.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -23,6 +31,26 @@ public class CategoryController {
     public ResponseResult listAllCategory(){
         List<CategoryVo> list = categoryService.listAllCategory();
         return ResponseResult.okResult(list);
+    }
+
+    @GetMapping("/export")
+    public void export(HttpServletResponse response){
+        try {
+            //设置下载文件的请求头
+            WebUtils.setDownLoadHeader("分类.xlsx",response);
+            //获取需要导出的数据
+            List<Category> categoryVos = categoryService.list();
+
+            List<ExcelCategoryVo> excelCategoryVos = BeanCopyUtils.copyBeanList(categoryVos, ExcelCategoryVo.class);
+            //把数据写入到Excel中
+            EasyExcel.write(response.getOutputStream(), ExcelCategoryVo.class).autoCloseStream(Boolean.FALSE).sheet("分类导出")
+                    .doWrite(excelCategoryVos);
+
+        } catch (Exception e) {
+            //如果出现异常也要响应json
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR);
+            WebUtils.renderString(response, JSON.toJSONString(result));
+        }
     }
 
 
